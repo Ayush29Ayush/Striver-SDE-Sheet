b240d76636ae0e0f507763c539f1641530200b7e
Ayush Senapati <ayushsenapati123@gmail.com>
2022-06-27 21:56:58 +0530

    merge overlapping subintervals

---
+++ Day-2-Arrays-part-2/2-LC-56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/merge-intervals/discuss/2205936/C%2B%2B-oror-DETAILED_EXPLAINATION-oror-BEATS-100-oror-OPTIMIZED
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        
+        vector<vector<int>> ans;
+        
+        if(intervals.size()==0) 
+            return ans;
+        
+        sort(intervals.begin(),intervals.end());
+        
+        vector<int> temp=intervals[0];
+        
+        for(auto it: intervals)
+        {
+            if(it[0]<=temp[1])
+            {
+                temp[1]=max(it[1],temp[1]);
+            }
+            else
+            {
+                ans.push_back(temp);
+                temp=it;
+            }
+        }
+        ans.push_back(temp);
+        return ans;
+    }
+};
