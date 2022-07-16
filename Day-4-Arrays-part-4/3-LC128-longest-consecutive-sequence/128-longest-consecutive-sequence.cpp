b7345148ffe19a1c11597e150e1b3072ae6514e9
Ayush Senapati <ayushsenapati123@gmail.com>
2022-07-16 22:14:28 +0530

    longest consecutive sequence

---
+++ Day-4-Arrays-part-4/3-LC128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/longest-consecutive-sequence/discuss/2239045/C%2B%2B-oror-BEATS-95-oror-O(N)-ororOPTIMIZED
+
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> s(nums.begin(),nums.end());
+        
+        // for(int i = 0; i<nums.size(); i++)
+        // {
+        //     s.insert(nums[i]);
+        // }
+        
+        int longest_sequence = 0;
+        
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(s.find(nums[i] - 1)!=s.end())
+                continue;
+            
+            else
+            {
+                int count = 0;
+                int current_element = nums[i];
+                
+                while(s.find(current_element) != s.end())
+                {
+                    count++;
+                    current_element++;
+                }
+                
+                longest_sequence = max(longest_sequence,count);
+            }
+        }
+        
+        return longest_sequence;
+    }
+};
